1da61c5633c818c0fa342975d3e0050731d8f6ee
Mike Alhayek <mike@crestapps.com>
2018-05-08 17:09:34 -0700

    Deletes the default views

--- MvcWithUnobtrusive/Controllers/HomeController.cs
+++ MvcWithUnobtrusive/Controllers/HomeController.cs
@@ -7,26 +7,6 @@ namespace MvcWithUnobtrusive.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-
-        public ActionResult Test()
-        {
             var viewModel = new CreatePerson();
 
             return View(viewModel);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MvcWithUnobtrusive
+
+This is a test repo to demonstrate a possible bug with `jquery-validation-unobtrusive` project.
+
+For more info about the potential bug, please visit https://github.com/aspnet/jquery-validation-unobtrusive/issues/88#issuecomment-387531538
